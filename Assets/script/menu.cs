5b80fc84dff1a2c2a7f5a005c2035c33cbf82224
Marie Chapard <mchapard@student.le-101.fr>
2020-09-14 18:10:39 +0200

    first commit

---
+++ Assets/script/menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour
+{
+    public GameObject canvas;
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            canvas.SetActive(true);
+            Time.timeScale = 0;
+            GameObject button = GameObject.Find("play");
+            button.GetComponentInChildren<UnityEngine.UI.Text>().text = "Play";
+        }
+    }
+    public void play()
+    {
+        //lancer scene suivante
+        canvas.SetActive(false);
+    }
+
+}
