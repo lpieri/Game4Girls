56dbdb82ce8649beb72f64516f2ea1f32c3aeecb
Marie Chapard <mchapard@student.le-101.fr>
2020-09-15 16:46:14 +0200

    defilement version1 ok

---
+++ Assets/script/paralaxe.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paralaxe : MonoBehaviour
+{
+    float offset;
+    public float vitesse;
+
+	void Update () 
+	{
+		float toMove = vitesse * Time.deltaTime;
+		transform.Translate(Vector3.up*toMove, Space.World);
+	}
+}
