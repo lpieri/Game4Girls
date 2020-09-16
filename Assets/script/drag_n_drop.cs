f758231c6162b087a6b08b28943f5cb96ba737c7
Marie Chapard <mchapard@student.le-101.fr>
2020-09-16 07:25:01 +0200

    wip2

---
+++ Assets/script/drag_n_drop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drag_n_drop : MonoBehaviour
+{
+    private bool isDragging;
+    private bool hasBeenDragged;
+
+    public void OnMouseDown(){
+        isDragging = true;
+    }
+
+    public void OnMouseUp(){
+        isDragging = false;
+        hasBeenDragged = true;
+    }
+
+    void Update()
+    {
+        if(isDragging && !hasBeenDragged){
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition);
+        }
+    }
+}
